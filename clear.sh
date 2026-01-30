eac28d519efbe4d7a05ebd4322648c395a0d4d57
calm <calm@calm.com>
2026-01-30 14:40:16 +0800

    .

---
+++ clear.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+
+rm -rf app/keys
+rm -rf app/node
---
+++ reset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+
+rm -rf app/keys
+rm -rf app/node
+rm -rf app/config/genesis.json
+rm -rf app/config/genesis.sha256
